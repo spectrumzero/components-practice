2e19aefbe1eaf797bc2e6537c28d1de9d6a2a3f0
Spectrumzero <lmhspecter@gmail.com>
2025-02-19 21:22:33 +0800

    star-rating

---
+++ star-rating/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "#source-map",
+};
